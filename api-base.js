50e358c686e3c9496009174219ae480c29b356fb
Kira <petty7244@gmail.com>
2026-08-18 11:37:51 +0800

    Set api base to localhost for local testing

--- api-base.js
+++ api-base.js
@@ -1,6 +1,6 @@
 // API base configuration for GitHub Pages deployment
 // Replace the empty string below with your public backend URL (eg. ngrok URL)
 // Example: window.__API_BASE__ = "https://abcd1234.ngrok.io";
-window.__API_BASE__ = "";
+window.__API_BASE__ = "http://localhost:8000";
 
 // NOTE: Do NOT commit a private/secret URL if you don't want it public.
